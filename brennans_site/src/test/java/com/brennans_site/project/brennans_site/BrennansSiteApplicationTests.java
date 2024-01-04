086e29db9d7c58cec4b3ff8d6f2896c748f14e71
Brennan Johnson <brennanMJ123@gmail.com>
2024-01-04 13:12:28 -0500

    Add files via upload

---
+++ brennans_site/src/test/java/com/brennans_site/project/brennans_site/BrennansSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brennans_site.project.brennans_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrennansSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
